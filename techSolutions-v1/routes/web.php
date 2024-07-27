3355788e1d82b41fcf5c85d914add6c6496d2a16
Laura Figueroa <laufigueroasalas@gmail.com>
2024-07-27 12:39:17 -0400

    dev: added sql database file, and added h1 to obtenerProyectoView

--- techSolutions-v1/routes/web.php
+++ techSolutions-v1/routes/web.php
@@ -7,7 +7,8 @@ Route::get('/', function () {
 });
 
 Route::get('/all-projects', function () {
-    return "Estos son todos los proyectos :)";
+    //return "Estos son todos los proyectos :)";
+    return view('obtenerProyectoView');
 });
 
 Route::get('/all-projects/{_id}', function ($_id) {
@@ -15,15 +16,15 @@ Route::get('/all-projects/{_id}', function ($_id) {
 });
 
 Route::post('/add-project/{_id}', function ($_id) {
-    return"Se ha añadido el proyecto número {$_id}";
+    return "Se ha añadido el proyecto número {$_id}";
 });
 
 Route::delete('/delete-project/{_id}', function ($_id) {
-    return"Se ha eliminado el proyecto número {$_id}";
+    return "Se ha eliminado el proyecto número {$_id}";
 });
 
 Route::put('/update-project/{_id}', function ($_id, $_value) {
-    return"Se ha actualizado el proyecto número {$_id} con el nuevo valor {$_value}.";
+    return "Se ha actualizado el proyecto número {$_id} con el nuevo valor {$_value}.";
 });
 
 
