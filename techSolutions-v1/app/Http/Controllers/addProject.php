85fcbf32e8b9662f49fbc0fad4fbe08be9e28249
LuzM-08 <martinezluzv09@gmail.com>
2024-07-27 23:28:22 -0400

    Errors corrected

---
+++ techSolutions-v1/app/Http/Controllers/addProject.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class addProject extends Controller
+{
+    public function add($_id, $_nuevo){
+        // insert into tabla values = $_id, $_nuevo; 
+    }
+}
--- techSolutions-v1/routes/web.php
+++ techSolutions-v1/routes/web.php
@@ -1,39 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UFController; 
-
+use App\Http\Controllers\getProject;
+use App\Http\Controllers\addProject;
+use App\Http\Controllers\updateProject;
+use App\Http\Controllers\deleteProject;
+use App\Http\Controllers\UFController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/all-projects', function () {
-    //return "Estos son todos los proyectos :)";
     return view('obtenerProyectoView');
 });
 
-Route::get('/all-projects/{_id}', function ($_id) {
-    return "Este es el proyecto número {$_id}";
-});
+Route::get('/all-projects/{_id}', [getProject::class, 'get']);
 
-Route::post('/add-project/{_id}', function ($_id) {
-    return "Se ha añadido el proyecto número {$_id}";
-});
+Route::post('/add-projects/{_id}', [addProject::class, 'add']);
 
-Route::delete('/delete-project/{_id}', function ($_id) {
-    return "Se ha eliminado el proyecto número {$_id}";
-});
+Route::put('/update-project/{_id}', [updateProject::class, 'add']);
 
-Route::put('/update-project/{_id}', function ($_id, $_value) {
-    return "Se ha actualizado el proyecto número {$_id} con el nuevo valor {$_value}.";
-});
+Route::delete('/delete-project/{_id}', [deleteProject::class, 'add']);
+
 
 // Para la API de UF
- 
-Route::get('/view-UF', [UFController::class,'getUF']);
 
-/*Route::get('/view-UF/{key}', function($_key){
-    Http::get("http://api.cmfchile.cl/api-sbifv3/recursos_api/uf?apikey={$_key}&formato=json");
-    return view('verUF');
-});*/
+Route::get('/view-UF', [UFController::class, 'getUF']);
+
